d6590138c1b0e7384c92eabfb842feb0f7303fad
Sahit21 <sahit2125@gmail.com>
2025-11-21 15:08:26 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ function App() {
       <Route path="/impressum" element={<ImpressumPage />} />
       <Route path="/datenschutz" element={<DatenschutzPage />} />
       <Route path="/kalender" element={<CalendarPageRoute />} />
-       <Route path="/agb" element={<CalendarPageRoute />} />
+       <Route path="/agb" element={<AgbPage />} />
     </Routes>
   );
 }
